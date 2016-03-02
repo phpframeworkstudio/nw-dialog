6954ce887e9b1db70bfea1b96a3ad28db70e0162
Dida Nurwanda <didanurwanda@gmail.com>
2016-03-02 12:00:22 +0700

    misplaced :D

--- nw-dialog/index.js
+++ nw-dialog/index.js
@@ -11,10 +11,7 @@ var nwdialog = {
     openFileDialog: function(filter, multiple, callback) {
 
         var fn          = callback;
-        var node        = this._context.getElementById('open-file-dialog');
-        if (node === null) {
-            node        = this._context.createElement('input');
-        }
+        var node        = this._context.createElement('input');
         node.type       = 'file';
         node.id         = 'open-file-dialog';
         node.style      = 'display: none';
@@ -22,60 +19,70 @@ var nwdialog = {
         if (typeof filter === 'function') {
             fn = filter;
         } else if (typeof filter === 'string') {
-            node.accept = filter;
+            node.setAttribute('accept', filter);
+        } else if (this.isArray(filter)) {
+            node.setAttribute('accept', filter.join(','));
+        } else if (typeof filter === 'boolean' && filter === true) {
+            node.setAttribute('multiple', '');
         }
 
         if (typeof multiple === 'function') {
             fn = multiple;
         } else if (typeof multiple === 'boolean' && multiple === true) {
-            node.multiple = multiple;
+            node.setAttribute('multiple', '');
+        } else if (typeof multiple === 'string') {
+            node.setAttribute('nwdirectory', ''); // not work in 0.13
         }
 
         this._context.body.appendChild(node);
         node.addEventListener('change', function() {
             fn(node.value);
+            node.remove();
         });
         node.click();
 
     },
 
-    saveFileDialog: function(name, directory, callback) {
+    saveFileDialog: function(name, accept, directory, callback) {
         
         var fn          = callback;
-        var node        = this._context.getElementById('save-file-dialog');
-        if (node === null) {
-            node        = this._context.createElement('input');
-        }
+        var node        = this._context.createElement('input');
         node.type       = 'file';
         node.id         = 'save-file-dialog';
         node.style      = 'display: none';
-        node.nwsaveas   = '';
+        node.setAttribute('nwsaveas', '');
 
         if (typeof name === 'function') {
             fn = name;
         } else if (typeof name === 'string') {
-            node.nwsaveas = name;
+            node.setAttribute('nwsaveas', name);
+        }
+
+        if (typeof accept === 'function') {
+            fn = accept;
+        } else if (typeof accept === 'string') {
+            node.setAttribute('accept', accept);
+        } else if (this.isArray(accept)) {
+            node.setAttribute('accept', accept.join(','));
         }
 
         if (typeof directory === 'function') {
             fn = directory;
         } else if (typeof directory === 'string') {
-            node.nwworkingdir = directory;
+            node.setAttribute('nwdirectory', directory); //not work in 0.13
         }
 
         this._context.body.appendChild(node);
         node.addEventListener('change', function() {
             fn(node.value);
+            node.remove();
         });
         node.click();
 
     },
 
     folderBrowserDialog: function(callback) {
-        var node        = this._context.getElementById('folder-browser-dialog');
-        if (node === null) {
-            node        = this._context.createElement('input');
-        }
+        var node        = this._context.createElement('input');
         node.type       = 'file';
         node.id         = 'folder-browser-dialog';
         node.style      = 'display: none';
@@ -84,8 +91,13 @@ var nwdialog = {
         this._context.body.appendChild(node);
         node.addEventListener('change', function() {
             callback(node.value);
+            node.remove();
         });
         node.click();
+    },
+
+    isArray: function(value) {
+      return Object.prototype.toString.call(value) === '[object Array]';
     }
 
 }
